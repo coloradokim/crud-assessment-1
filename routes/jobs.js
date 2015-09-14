1bcb8203591bd5723f1e085b2255fa0ac8e19110
Kim Schlesinger <schlesinger.kim@gmail.com>
2015-09-14 09:55:51 -0600

    create route and views working

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var db = require('monk')('localhost/jobs-demo');
+var jobCollection = db.get('jobs');
 
-/* GET home page. */
 router.get('/jobs', function(req, res, next) {
   res.render('jobs/index');
+});
+
+router.get('/jobs/new', function(req, res, next) {
+  res.render('jobs/new');
+});
+
+router.post('/jobs', function(req, res, next) {
+  jobCollection.insert({
+    title: req.body.title,
+    company: req.body.company,
+    description: req.body.description,
+    responsibilities: req.body.responsbilities,
+    timeStamp: new Date(),
+    open: true});
+  res.redirect('/jobs')
 });
 
 module.exports = router;
